5eb03dc9d6328bf9fea5d72195d9d12a95f42228
mattk111 <72532026+mattk111@users.noreply.github.com>
2022-01-02 20:55:41 -0500

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,15 @@
+def SwapFileData(): 
+        file1 = input("enter the file name: ")
+        file2 = input("enter the file name: ")
+
+        with open(file1, 'r') as a:
+            data_a = a.read()
+        with opem(file2, 'r') as b:
+            data_b = b.read()
+
+        with open(file1, 'w') as a:
+            a.write(data_b)
+        with open(file1, 'w') as b:
+            b.write(data_a)
+
+    SwapFileData();
